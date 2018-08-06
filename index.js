c6624562b07e8d175fc16199126d2856c2c62593
Katarzyna Kensy <zbierek@gmail.com>
2018-08-06 22:01:08 +0200

    Change index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 var color = require('colors');
 
-fs.readdir('./','utf-8', function(err, data){
+fs.readdir('./', 'utf-8', function(err, data) {
     if (err) throw err;
     console.log('Zawartość katalogu:'.rainbow)
     console.log(data);
-    fs.writeFile('./new.txt','\n' + data, function(err){
-      if (err) throw err;
-      console.log('Zapisano!'.blue);
+    fs.writeFile('./new.txt', '\n' + data, function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
     });
 });
